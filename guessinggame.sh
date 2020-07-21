54b7adc159d954bc1f0020a07fb73adf019629f0
Harini456 <68548516+Harini456@users.noreply.github.com>
2020-07-21 13:31:53 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+O#!/usr/bin/env bash
+
+echo "[Guessing game 101 ]"
+
+function ask {
+	echo "Do you know your device ? Try and guess the number of files in the current directory:"
+	read guess
+    files=$(ls -1 | wc -l)
+}
+
+ask
+
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+		echo "Too low."
+		echo " "
+	else
+		echo "Too high."
+		echo " "
+	fi
+	ask
+done
+
+echo " "
+echo "Congratulations,you have got it right. This is the list:"
+echo " "
+echo "---" && ls -1
